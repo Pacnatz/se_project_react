5f9f7819aedd2fa55dbc7d22eb8fb39bdd9053e4
Pacnatz <nhatchu04@yahoo.com>
2025-11-20 00:13:14 -0600

    style: itemcard

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+
   return (
     <div className="app">
       <div className="app__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
